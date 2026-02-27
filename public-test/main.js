fa95cf0288f3056171f3913bab619f331c957eb7
JJ2229 <valentijnjj@gmail.com>
2026-02-27 12:58:07 +0100

    added sound

--- public-test/main.js
+++ public-test/main.js
@@ -4,6 +4,9 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 let model;                // je 3D model
 let targetRotation = 0;   // scroll target
 let currentRotation = 0;
+const clickSound = new Audio('audio/Mahoraga Wheel Spin  Adaption Sound Effect.mp3');
+let canPlaySound = true; // true = geluid mag afspelen
+const soundCooldown = 1000; // 1000 ms = 1 seconde cooldow
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -61,10 +64,10 @@ function updateModelScale() {
 
     if(width > 1200) {
         model.scale.set(2,2,2);
-    } else if(width > 800) {
-        model.scale.set(0.7,0.7,0.7);
+    } else if(width > 786) {
+        model.scale.set(1.5,1.5,1.5);
     } else {
-        model.scale.set(0.5,0.5,0.5);
+        model.scale.set(1.2,1.2,1.2);
     }
 }
 
@@ -88,6 +91,26 @@ window.addEventListener("scroll", () => {
 });
 
 
+function isNearBottom() {
+    // window.innerHeight = viewport hoogte
+    // window.scrollY = huidige scroll top
+    // document.body.scrollHeight = totale pagina hoogte
+    return window.scrollY + window.innerHeight >= document.body.scrollHeight - 50;
+}
+
+window.addEventListener("scroll", () => {
+    // scroll-rotation van je model
+    targetRotation = window.scrollY * 0.005;
+
+    // check of we near bottom zijn en cooldown actief
+    if(isNearBottom() && canPlaySound){
+        clickSound.play();    // speel geluid
+        canPlaySound = false; // blokkeer tijdelijk
+        setTimeout(() => canPlaySound = true, soundCooldown); // reset na cooldown
+    }
+});
+
+
 function animate() {
     requestAnimationFrame(animate);
 
@@ -100,3 +123,37 @@ function animate() {
 }
 
 animate();
+
+
+const projectItems = document.querySelectorAll(".reveal-project");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show"); // verdwijnt bij omhoog scrollen
+    }
+  });
+}, { threshold: 0.25 });
+
+projectItems.forEach(item => observer.observe(item));
+
+const modal = document.getElementById("contactModal");
+const btn = document.getElementById("contactBtnNav");
+const close = document.querySelector(".close");
+
+btn.onclick = () => {
+  modal.style.display = "block";
+};
+
+close.onclick = () => {
+  modal.style.display = "none";
+};
+
+window.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+};
+
